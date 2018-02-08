9506145f5ed37e70e9f47a264351b58fa95d23ab
Ogie Sado <sado.ogie@gmail.com>
2018-02-08 19:19:45 +0000

    create workspace api

---
+++ server/controllers/workspaces.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto';
+import env from '../utils/env';
+import { getRedisClient } from '../utils/redis';
+import {
+  respondServerError,
+  respondOk,
+  respondBadRequest,
+} from '../utils/responses';
+import { isValidWorkspaceId } from '../../shared/utils';
+
+export function createWorkpace(req, res) {
+  const id = req.body.id;
+
+  if (!isValidWorkspaceId(id)) {
+    return respondBadRequest(res, 'Invalid workspace ID');
+  }
+
+  const key = crypto
+    .createHmac('sha256', env('APP_KEY'))
+    .update(String(Date.now()))
+    .digest('hex');
+
+  getRedisClient()
+    .hset('WORKSPACE', key, id)
+    .then(() => respondOk(res, { id, key }))
+    .catch(error => respondServerError(res, error.message));
+}
